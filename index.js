f3c0dab27a0e728340203d4e606f1d3cf39f4761
Nahidul Islam <nahid@Nahiduls-MacBook-Pro.local>
2019-06-26 12:45:45 +0300

    Add morgan, show data in log on post request and add middleware for invalid endpoint.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParse = require('body-parser')
+const morgan = require('morgan')
 
 const app = express()
 const PORT = 3003
-const persons = [
+let persons = [
       {
         "name": "Arto Hellas",
         "number": "123",
@@ -18,69 +19,49 @@ const persons = [
         "name": "Dan Abramov",
         "number": "12-43-234345",
         "id": 3
-      },
-      {
-        "name": "Artho Hellas",
-        "number": "451",
-        "id": 4
-      },
-      {
-        "name": "Nahidul Islam",
-        "number": "123456",
-        "id": 5
-      },
-      {
-        "name": "Ashraful Alam",
-        "number": "123456",
-        "id": 6
-      },
-      {
-        "name": "Kamal Ahmed",
-        "number": "123456",
-        "id": 7
-      },
-      {
-        "name": "Alif",
-        "number": "1244",
-        "id": 8
-      },
-      {
-        "name": "rajon",
-        "number": "2343",
-        "id": 9
-      },
-      {
-        "name": "Ash",
-        "number": "123",
-        "id": 10
-      },
-      {
-        "name": "Km",
-        "number": "2343",
-        "id": 11
-      },
-      {
-        "name": "Hii",
-        "number": "1234",
-        "id": 12
       }
     ]
-app.use(bodyParse.json())
 
+
+app.use(bodyParse.json())
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    return res.json(persons)
 })
 
 app.get('/info', (req, res) => {
-    res.send(
+    return res.send(
         `<p>Phonebook has the info of ${persons.length} persons.</p>
          <p>${new Date()}</p>`
     )
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    res.json(persons.find(person => person.id === Number(req.params.id)))
+    return res.json(persons.find(person => person.id === Number(req.params.id)))
 })
+
+app.post('/api/persons', (req, res) => {
+    if(!req.body.name)
+        return res.status(400).json({error: 'Name is missing'})
+    if(!req.body.number)
+        return res.status(400).json({error: 'Number is missing'})
+    if(persons.find(person => person.name === req.body.name))
+        return res.status(400).json({error: 'Name should be unique'})
+    persons = [...persons, {...req.body, id: Math.round(Math.random() * 100)}]
+    return res.status(204).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    persons = persons.filter(person => person.id !== Number(req.params.id))
+    return res.status(204).end()
+})
+
+const unKnownEndPoint = (req, res) => {
+    res.status(404).json({error: 'invalid endpoint.'})
+}
+
+app.use(unKnownEndPoint)
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`)
